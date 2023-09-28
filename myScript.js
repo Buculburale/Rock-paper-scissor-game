79e4c39d96b19142548690aee0cfb48bf0ef65de
bucul <abdulrahman2010.ali@gmail.com>
2023-09-28 08:04:21 +0300

    update the initial JS file after adding UI.

--- myScript.js
+++ myScript.js
@@ -1,50 +1,91 @@
+// Initialize player and computer scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Get references to the HTML elements
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const resultDiv = document.querySelector('.result');
+const playerScoreSpan = document.querySelector('.player-score');
+const compueterScoreSpan = document.querySelector('.computer-score');
+
+
 // Function to randomly choose the computer's move
 function getComputerChoice() {
   const choice = ['rock', 'paper', 'scissors'];
   return choice [Math.floor(Math.random() * choice.length)];
 }
 
+
 // Function to determine the result of a single round
 function playRound(playerSelection, computerSelection) {
+
+  // Create an element to display the round result
+  const resultMessage = document.createElement('p');   
+
+  // Check for the outcome of the round and update scores
   if (playerSelection === computerSelection) {
-    return "it is a tie";
+    resultMessage.innerText = "it is a tie";
   }else if (playerSelection==="rock" && computerSelection === "paper") {
-    return "computer wins"
+    resultMessage.innerText = "computer wins";
+    computerScore++;
   }else if (playerSelection === "paper" && computerSelection == "scissors") {
-    return "compueter wins"
+    resultMessage.innerText = "compueter wins";
+    computerScore++;
   }else if (playerSelection == "scissors" && computerSelection === "rock") {
-    return "computer wins"
+    resultMessage.innerText = "computer wins";
+    computerScore++;
   }else{
-    return "player wins"
+    resultMessage.innerText = "player wins";
+    playerScore++
   }
+
+    // Display the round result
+  resultDiv.appendChild(resultMessage);
          
 }
 
-// Function to manage the game
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i <= 4; i++) {
-    const playerSelection = prompt("enter your choice: rock, paper or scissors").toLocaleLowerCase();
-    const computerSelection = getComputerChoice()
-    const result = playRound(playerSelection, computerSelection);
-    console.log(`Round ${i + 1}: ${result}`);
-  if (result==="players wins") {
-    playerScore++;
-  }else if (result==="computer wins") {
-    computerScore++;
+    // Create an element to display the game winner
+const winnerMessage = document.createElement('h4')
+
+    // Function to announce the game winner
+const anounceWinner = (playerScore, computerScore) =>{
+  if (playerScore === 5) {
+    winnerMessage.innerText = 'Player won the game'
+  }else if (computerScore === 5) {
+    winnerMessage.innerText = 'computer won the game'
   }
 
-  // Determine the winner of the game
-  if (playerScore > computerScore) {
-    console.log("player wins the game");
-  }else if (computerScore > playerScore) {
-    console.log("computer wins the game");
-  }else{
-    console.log("it is a tie");
-  }
 }
+//Function to display the running score.
+const updateScore =(playerScore, computerScore) =>{
+  playerScoreSpan.innerText = `Player score: ${playerScore}`;
+  compueterScoreSpan.innerText =`Computer score: ${computerScore}`;
 }
 
-// Start the game
-game();
+    // Event listeners for player's choices
+rockButton.addEventListener('click', () => {
+  const playerSelection = 'rock';
+  const computerSelection = getComputerChoice();
+  playRound(computerSelection, playerSelection);
+  updateScore(playerScore, computerScore);
+  anounceWinner(playerScore, computerScore);
+});
+
+paperButton.addEventListener('click', () => {
+  const playerSelection = 'paper';
+  const computerSelection = getComputerChoice();
+  playRound(computerSelection, playerSelection);
+  anounceWinner(playerScore, computerScore);
+});
+
+scissorsButton.addEventListener('click', () => {
+  const playerSelection = 'scissors';
+  const computerSelection = getComputerChoice();
+  playRound(computerSelection, playerSelection);
+  anounceWinner(playerScore, computerScore);
+});
+
+    // Append the game winner message element to the result div
+resultDiv.appendChild(winnerMessage);
